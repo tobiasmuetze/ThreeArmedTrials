14cffc04396e3ccfca885799711feecfe7a08dd2
Tobias Muetze <git@tobiasmuetze.com>
2016-01-06 13:36:13 +0100

    Version 0.3-0

--- inst/Simulation-Studies/Permutation-Test_Type-I-Error.R
+++ inst/Simulation-Studies/Permutation-Test_Type-I-Error.R
@@ -1,60 +1,42 @@
+rm(list = ls())
 library(ThreeArmedTrials)
-library(xtable)
 
-# Poisson
-results_poisson <- data.frame()
-# 1:1:1
-out <- simulation.permutation.test(parameterExp=list(n = 33, lambda = 0.72),distExp='rpois',
-                                   parameterRef=list(n = 33, lambda = 0.34), distRef='rpois',
-                                   parameterPla=list(n = 33, lambda = 1.5), distPla='rpois',
-                                   nSimulations=15000, nPermutations=10000, Delta=78/116, sig.level=0.025)
-results_poisson <- rbind(results_poisson, out)
+parameters <- read.table(file = "/home/tmuetze/Dropbox/R-Packages/ThreeArmedTrials/inst/Simulation-Studies/Permutation-Test_Type-I-Error_Scenarios.txt", header = TRUE, sep = "\t")
 
-# 3:2:1
-out <- simulation.permutation.test(parameterExp=list(n = 51, lambda = 0.72),distExp='rpois',
-                                   parameterRef=list(n = 34, lambda = 0.34), distRef='rpois',
-                                   parameterPla=list(n = 17, lambda = 1.5), distPla='rpois',
-                                   nSimulations=15000, nPermutations=10000, Delta=78/116, sig.level=0.025)
-results_poisson <- rbind(results_poisson, out)
+parameters_large <- parameters
+parameters_large$rate_exp <- parameters_large$rate_exp * 3
+parameters_large$rate_ref <- parameters_large$rate_ref * 3
+parameters_large$rate_pla <- parameters_large$rate_pla * 3
 
-# 2:2:1
-out <- simulation.permutation.test(parameterExp=list(n = 40, lambda = 0.72),distExp='rpois',
-                                   parameterRef=list(n = 40, lambda = 0.34), distRef='rpois',
-                                   parameterPla=list(n = 20, lambda = 1.5), distPla='rpois',
-                                   nSimulations=15000, nPermutations=10000, Delta=78/116, sig.level=0.025)
-results_poisson <- rbind(results_poisson, out)
+parameters_final <- rbind(parameters, parameters_large)
+parameters_final$rate_ref <- (parameters_final$rate_exp - (1 - parameters_final$Delta) * parameters_final$rate_pla) / parameters_final$Delta
 
 
-results_poisson
-xtable(results_poisson, digits = c(0, 0, 2, 0, 2, 0, 2, 5))
+results <- data.frame()
 
+for (i in seq_along(parameters_final[,1])) {
 
-# Binary
-results_binary <- data.frame()
-# 1:1:1
-out <- simulation.permutation.test(parameterExp = list(n = 33, size = 1, prob = 0.35), distExp='rbinom',
-                                   parameterRef = list(n = 33, size = 1, prob = 0.2), distRef='rbinom',
-                                   parameterPla = list(n = 33, size = 1, prob = 0.7), distPla='rbinom',
-                                   nSimulations = 15000, nPermutations = 10000, Delta = 0.7, sig.level=0.025)
-results_binary <- rbind(results_binary, out)
+  n_exp <- parameters_final$n_exp[i]
+  rate_exp <- parameters_final$rate_exp[i]
+  n_ref <- parameters_final$n_ref[i]
+  rate_ref <- parameters_final$rate_ref[i]
+  n_pla <- parameters_final$n_pla[i]
+  rate_pla <- parameters_final$rate_pla[i]
+  Delta <- parameters_final$Delta[i]
 
-# 3:2:1
-out <- simulation.permutation.test(parameterExp = list(n = 51, size = 1, prob = 0.35), distExp='rbinom',
-                                   parameterRef = list(n = 34, size = 1, prob = 0.2), distRef='rbinom',
-                                   parameterPla = list(n = 17, size = 1, prob = 0.7), distPla='rbinom',
-                                   nSimulations = 15000, nPermutations = 10000, Delta = 0.7, sig.level=0.025)
-results_binary <- rbind(results_binary, out)
+  out <- simulate_test_RET_permutation(parameterExp = list(n = n_exp, lambda = rate_exp),
+                                       distExp = 'rpois',
+                                       parameterRef = list(n = n_ref, lambda = rate_ref),
+                                       distRef = 'rpois',
+                                       parameterPla = list(n = n_pla, lambda = rate_pla),
+                                       distPla = 'rpois',
+                                       nSimulations = 50000,
+                                       nPermutations = 25000,
+                                       Delta = Delta,
+                                       sig.level = 0.025,
+                                       competitor = TRUE)
+  results <- rbind(results, out)
+}
 
-# 2:2:1
-out <- simulation.permutation.test(parameterExp = list(n = 40, size = 1, prob = 0.35), distExp='rbinom',
-                                   parameterRef = list(n = 40, size = 1, prob = 0.2), distRef='rbinom',
-                                   parameterPla = list(n = 20, size = 1, prob = 0.7), distPla='rbinom',
-                                   nSimulations = 15000, nPermutations = 10000, Delta = 0.7, sig.level=0.025)
-results_binary <- rbind(results_binary, out)
+results
 
-
-results_binary
-xtable(results_binary, digits = c(0, 0, 0, 2, 0, 0, 2, 0, 0, 2, 5))
-
-
-0.025 + 2*sqrt(0.025*0.975/15000)
